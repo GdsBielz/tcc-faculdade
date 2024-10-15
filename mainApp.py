529c97e6daa56b68e9abc1ff7516b2fea58826fa
Gabriel Sales <gdschannelpvp@gmail.com>
2024-10-15 16:50:43 -0300

    design home

--- mainApp.py
+++ mainApp.py
@@ -37,4 +37,4 @@ def page_not_found(e):
 
 #Para iniciar quando executar o arquivo
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3000, debug=True)
